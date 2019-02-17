f1eefa0ff0c6b72dd6a02aa6c71fa2ce5f88d7a6
AshimPandey <ashimpandey2015@gmail.com>
2019-02-18 00:41:15 +0530

    New Project

---
+++ printName/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		String name=scan.next();
+	
+		
+		System.out.println("My name is "+name);
+	}
+}
